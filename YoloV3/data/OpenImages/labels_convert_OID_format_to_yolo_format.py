17cc59b996333bce61893019229072680b568f0a
amit <amit.nativ@elbitsystems.com>
2020-02-16 11:29:10 +0200

    updating source path and bbox normalazied coordinates

--- YoloV3/data/OpenImages/labels_convert_OID_format_to_yolo_format.py
+++ YoloV3/data/OpenImages/labels_convert_OID_format_to_yolo_format.py
@@ -5,24 +5,29 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    source_path = "/home/amit/Data/OID/OID/noGroup/train/labels_org"
-    dest_path = "/home/amit/Data/OID/OID/noGroup/train/labels"
+    source_path = "/home/amit/Data/OID/OID/Dataset/train/Person_Ambulance_Bus_Truck_Car_Tank_Taxi/Label"
+    dest_path = source_path.replace("Label", "labels")
+
+    os.makedirs(dest_path, exist_ok=False)
 
     all_label_files = os.listdir(source_path)
 
     cls_dict = {
-                "Window": 0,
-                "Door": 1,
-                "Person": 2,
-                "House": 3,
-                "Car": 4
+                "Person": 0,
+                "Ambulance": 1,
+                "Bus": 1,
+                "Truck": 1,
+                "Car": 1,
+                "Tank": 1,
+                "Taxi": 1
                 }
+
     pbar = tqdm(all_label_files, ncols=100)
     pbar.set_description('files converted')
     for label_file in pbar:
         if label_file.endswith('.txt'):
             with open(os.path.join(source_path, label_file), 'r') as label:
-                img_file = os.path.join(dest_path.replace("labels", "images"),label_file.replace(".txt", ".jpg"))
+                img_file = os.path.join(source_path.replace("Label", "images"), label_file.replace(".txt", ".jpg"))
                 if not os.path.isfile(img_file):
                     continue
 
@@ -43,12 +48,12 @@ if __name__ == "__main__":
 
                     x0 = np.mean([xmin, xmax]) / W
                     y0 = np.mean([ymin, ymax]) / H
-                    w = (xmax - xmin) / W
-                    h = (ymax - ymin) / H
+                    w = (xmax - xmin + 1) / W
+                    h = (ymax - ymin + 1) / H
 
                     # # validating bbox size:
-                    # w = min(w, 2 * (1 - x0 - 0.001), 2 * x0)
-                    # h = min(h, 2 * (1 - y0 - 0.001), 2 * y0)
+                    w = min(w, 2 * (1 - x0))
+                    h = min(h, 2 * (1 - y0))
 
                     new_line = ' '.join([str(cls), str(x0), str(y0), str(w), str(h)])
                     bbox.append(new_line)
